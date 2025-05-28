0e4919c8a1de9ce10511b184d20c99850a5e763a
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 13:30:52 +0000

    hhh

--- api/hello.js
+++ api/hello.js
@@ -3,7 +3,6 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default function handler(req, res) {
-  res.status(200).json({ message: 'there is a message'});
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
