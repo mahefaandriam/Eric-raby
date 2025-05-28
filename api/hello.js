2aa787b2e2bc3b3aeebb38362b37167fed8c9ec6
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 16:00:40 +0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,9 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+consol.log("keyl:" + process.env.RESEND_API_KEY);
+
 export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello from the backend!' });
+  res.status(200).json({ message: 'Hello from the backend!' + process.env.RESEND_API_KEY});
 }
