82da659af227d848cf4d8e2471774fa2b0829912
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 17:18:43 +0000

    df

--- api/hello.js
+++ api/hello.js
@@ -2,10 +2,9 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default function handler(req, res) {
-
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' , method: req.method});
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
   const { name, email, message } = req.body;
@@ -27,5 +26,4 @@ export default function handler(req, res) {
     console.error('Resend error:', error);
     return res.status(500).json({ error: 'Failed to send email' });
   }
-
 }
