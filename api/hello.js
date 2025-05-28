f284a11fa04bf23352b1d531b86dc80e50cff47b
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 13:44:16 +0000

    sqs

--- api/hello.js
+++ api/hello.js
@@ -4,13 +4,14 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default function handler(req, res) {
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' , method: req.method});
-  }
+  return res.status(400).json({error: req.method , m: req.email});
+  //if (req.method !== 'POST') {
+    //return res.status(405).json({ error: 'Method Not Allowed' , method: req.method});
+  //}
 
  // const { name, email, message } = req.body;
 
-  return res.status(400).json({error: "good"});
+  
 
   if (!name || !email || !message) {
     return res.status(400).json({ error: 'All fields are required' });
