88d0ae30b3782967e9cfed0fa239882847c37b9b
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 13:26:53 +0000

    cxv

--- api/hello.js
+++ api/hello.js
@@ -11,6 +11,8 @@ export default function handler(req, res) {
 
   const { name, email, message } = req.body;
 
+  return res.status(400).json({error: email});
+
   if (!name || !email || !message) {
     return res.status(400).json({ error: 'All fields are required' });
   }
