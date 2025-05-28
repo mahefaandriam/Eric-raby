20a0014a129e1912108a169d2e76db43ba337f44
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-28 13:13:46 +0000

    hh

--- api/hello.js
+++ api/hello.js
@@ -2,8 +2,6 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-consol.log("keyl:" + process.env.RESEND_API_KEY);
-
 export default function handler(req, res) {
   res.status(200).json({ message: 'there is a message'});
 }
