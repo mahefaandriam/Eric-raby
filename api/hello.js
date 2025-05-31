1cdb3ea2232cea02e926dbf43845cbf92bef4215
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2025-05-31 09:43:04 +0300

    cv

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-//import ForwardedMessageEmail from '../src/emails/ForwardedMessageEmail'
+import ForwardedMessageEmail from '../src/emails/ForwardedMessageEmail';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
--- src/emails/ForwardedMessageEmail.jsx
+++ src/emails/ForwardedMessageEmail.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function ForwardedMessageEmail({ logoUrl, recipientName, fromName, fromEmail, subject, message }) {
   return (
     <div
