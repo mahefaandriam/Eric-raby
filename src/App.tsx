988de9f52cc742495adede678c23e9e67811a4a1
Mahefa andriam <fenoandriams@gmail.com>
2025-06-28 20:34:25 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import Test from './pages/Test';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/test" element={<Test />} />
           </Routes>
         </main>
       </div>
