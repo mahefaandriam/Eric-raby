48f8b92b85cafd0e7d3491e4f61336f098403bc2
Mahefa andriam <fenoandriams@gmail.com>
2025-08-06 17:50:53 +0300

    h

--- api/download.js
+++ api/download.js
@@ -2,14 +2,14 @@ import fs from 'fs'
 import path from 'path'
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'secure', 'my-book.html')
+  const filePath = path.join(process.cwd(), 'secure', 'my-book.pdf')
 
   if (!fs.existsSync(filePath)) {
     return res.status(404).json({ error: 'File not found' })
   }
 
   res.setHeader('Content-Type', 'application/pdf')
-  res.setHeader('Content-Disposition', 'attachment; filename="my-book.html"')
+  res.setHeader('Content-Disposition', 'attachment; filename="my-book.pdf"')
 
   const fileStream = fs.createReadStream(filePath)
   fileStream.pipe(res)
